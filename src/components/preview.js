3bb9fb70bb537d81bd9e426e637b3b999d2a224c
Olivier MONNIER <monnier@indigo-net.com>
2018-07-26 11:53:12 +0200

    get logs in iframe

--- src/components/preview.js
+++ src/components/preview.js
@@ -19,14 +19,43 @@ function transformHtml(html, js, css) {
     `;
 }
 
+function getIframeHtml(html, js, css) {
+  return `
+    <html>
+      <head>
+        <style>
+          ${css}
+        </style>
+      </head>
+      <body>
+        ${html}
+        <script>
+          methods = ['log', 'error', 'warn'];
+          methods.forEach(function(method) {
+            var oldMethod = console[method];
+            console[method] = function(m) {
+              parent.console[method](m);
+              oldMethod.apply(console, arguments);
+            } 
+          });
+        </script>
+        <script>
+          ${js}
+        </script>
+      </body>
+    </html>
+    `;
+}
+
 function Preview(sources) {
   const props$ = sources.props.startWith({ html: '', js: '', css: '' });
-  const html$ = props$.map(({ html, js, css }) => transformHtml(html, js, css))
-  const vdom$ = html$.map(html =>
+  const html$ = props$.map(({ html, js, css }) => transformHtml(html, js, css));
+  const iframeHtml$ = props$.map(({ html, js, css }) => getIframeHtml(html, js, css));
+  const vdom$ = iframeHtml$.map(html =>
     iframe({
       attrs: {
         srcdoc: html,
-        sandbox: "allow-scripts"
+        sandbox: "allow-scripts allow-same-origin"
       }
     })
   )
