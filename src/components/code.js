566a5f5d84e11ec783cb9acc0adedb5c0ae0c8dd
Olivier MONNIER <monnier@indigo-net.com>
2018-08-01 16:59:22 +0200

    codemirror on console

--- src/components/code.js
+++ src/components/code.js
@@ -1,8 +1,10 @@
 import xs from 'xstream';
-import { pre, code, div, span } from '@cycle/dom';
+import delay from 'xstream/extra/delay';
+import { div, span } from '@cycle/dom';
 import isolate from '@cycle/isolate';
 
 function Code(sources) {
+  let editor;
   const props$ = sources.props;
   const html$ = sources.html;
   const vdom$ = xs.combine(html$, props$).map(([html, props]) =>
@@ -10,8 +12,25 @@ function Code(sources) {
       attrs: {
         style: !props.visible ? 'display: none;' : ''
       }
-    }, [span('.label', props.label), pre(code('.html', html))])
+    }, [span('.label', props.label), div('.html')])
   )
+
+  xs.combine(props$, html$).filter(([props, html]) => props.visible).compose(delay(100)).take(1).addListener({
+    next: ([props, html]) => {
+      editor = CodeMirror(document.querySelector('.html'), {
+        lineNumbers: true,
+        tabSize: 2,
+        value: html,
+        readOnly: true,
+        mode: 'htmlmixed'
+      });
+    }
+  });
+
+  html$.addListener({
+    next: html => editor && editor.setValue(html)
+  });
+
 
   return {
     DOM: vdom$
